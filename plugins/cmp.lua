18e38e9959d3bdde7302d2c59249295a42a7269f
Mark Rikhter <markrihter98@gmail.com>
2022-09-24 14:23:27 +0700

    snippet engine added

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,16 +1,19 @@
-local lspkind = require('lspkind')
+local lspkind = require("lspkind")
 
-local cmp = require('cmp')
+local status, cmp = pcall(require, "cmp")
+if not status then
+	return
+end
 
-cmp.setup {
+cmp.setup({
 	window = {
-    completion = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
-  },
+	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-j>'] = cmp.mapping.select_next_item(),
-		['<C-k>'] = cmp.mapping.select_prev_item(),
-		['<Tab>'] = cmp.mapping.confirm({ select = true }),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<Tab>"] = cmp.mapping.confirm({ select = true }),
 		-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
 		-- ['<C-Space>'] = cmp.mapping.complete(),
@@ -18,14 +21,20 @@ cmp.setup {
 		-- ['<CR>'] = cmp.mapping.confirm({ select = true }),
 	}),
 	sources = {
-		{name = 'nvim_lsp', max_item_count = 10 },
-		{name = 'buffer', max_item_count = 10 },
-		{name = 'path', max_item_count = 10 },
+		{ name = "nvim_lsp", max_item_count = 10 },
+		{ name = "buffer", max_item_count = 10 },
+		{ name = "path", max_item_count = 10 },
+		{ name = "luasnip", max_item_count = 10 },
 	},
 	formatting = {
-		format = lspkind.cmp_format{}
-	}
-}
+		format = lspkind.cmp_format({}),
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
